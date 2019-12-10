f449267f346b45e3dff260ea4cf5fcf43a336a38
Alejandro Cardozo <uri.cardozo@gmail.com>
2019-12-10 18:14:55 -0300

    nodemon setup + first routes and controllers created

---
+++ src/controllers/booksController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+class BooksController {
+  public index(req: Request, res: Response): void {
+    res.render("books/index", {
+      title: "Books"
+    });
+  }
+
+  public renderFormBook(req: Request, res: Response): void {
+    res.render("books/add", {
+      title: "Add a Book"
+    });
+  }
+}
+
+export const booksController = new BooksController();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 import express from "express";
+import exphbs from "express-handlebars";
+import path from "path";
+
+// Imported routes
+import IndexRoutes from "./routes";
+import BooksRoutes from "./routes/books";
 
 // initializations
 const app = express();
 
 // settings
-app.set("port", 4000);
+app.set("port", process.env.PORT || 5000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    extname: ".hbs",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    helpers: require("./lib/helpers"),
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", ".hbs");
 
 // middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
+app.use("/", IndexRoutes);
+app.use("/books", BooksRoutes); //not .get bc we're importing an object instead a single route
 
 // static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // runserver
 app.listen(app.get("port"), () => {
